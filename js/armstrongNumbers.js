f891d438da16968379e726157a37d4f0f323e782
SHAWN_SMITH <141180011+ReginaFera13@users.noreply.github.com>
2024-02-02 18:02:38 -0500

    save progress

--- js/armstrongNumbers.js
+++ js/armstrongNumbers.js
@@ -1,5 +1,23 @@
 // How can you make this more scalable and reusable later?
 
 exports.findArmstrongNumbers = function() {
-
+    
 };
+
+function findArmstrongNumbers(num) {
+    let strNum = num.toString();
+    let digits = [];
+    let answer = 0;
+
+    for (let i = 0; i < strNum.length; i++) {
+        digits.push(parseInt(strNum[i]));
+    }
+
+    for (let i = 0; i < digits.length; i++) {
+        answer += digits[i] * strNum.length;
+    }
+    
+    return answer
+}
+
+console.log(findArmstrongNumbers(16))

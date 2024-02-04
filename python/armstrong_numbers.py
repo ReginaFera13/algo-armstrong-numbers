e66c80d48a90a528667be4631c50b266e3c0bc73
ReginaFera13 <shawnmariesmith@icloud.com>
2024-02-04 14:52:16 -0500

    solved

--- python/armstrong_numbers.py
+++ python/armstrong_numbers.py
@@ -1,2 +1,30 @@
 # How can you make this more scalable and reusable later?
+import math
+
 def find_armstrong_numbers(numbers):
+   answer = [] 
+   for num in numbers:
+       num_compare = num
+       str_num = str(num)
+       digits = []
+       final_num = 0
+       print('num_compare:', num_compare)
+       print('str_num:', str_num)
+       
+       for digit in str_num:
+           digits.append(int(digit))
+           print('digits:', digits)
+           
+       for digit in digits:
+           final_num += math.pow(digit, len(digits))
+           print('final_num:', final_num)
+           
+       if final_num == num_compare:
+           answer.append(int(final_num))
+
+   return answer
+     
+
+find_armstrong_numbers()
+
+
